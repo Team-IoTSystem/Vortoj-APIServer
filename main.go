ca3478699fd7b52b9c233f0560555a2c8e3d6da5
take <hayatake396@gmail.com>
2017-12-19 04:30:13 +0900

    apiの追加およびエンドポイントの変更

--- main.go
+++ main.go
@@ -16,16 +16,33 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return controllers.ConnectDB(c)
+		return controllers.RootConnect(c)
 	})
-	e.GET("/Packet/:id", func(c echo.Context) error {
-		return controllers.SelectPacketData(c)
+
+	e.GET("/api", func(c echo.Context) error {
+		return controllers.RootConnect(c)
 	})
-	e.GET("/Packet/new", func(c echo.Context) error {
-		return controllers.NewPacketData(c)
+
+	//packet
+	e.GET("/api/packet/:id", func(c echo.Context) error {
+		return controllers.PacketDataSelectID(c)
 	})
-	e.POST("/InsertDB", func(c echo.Context) error {
-		return controllers.InsertDB(c)
+	e.GET("/api/packet/new", func(c echo.Context) error {
+		return controllers.PacketDataSelectNew(c)
+	})
+	e.GET("/api/packet/macaddress", func(c echo.Context) error {
+		return controllers.PacketDataSelectMacAddress(c)
+	})
+
+	//distance
+	e.GET("/api/distance/:id", func(c echo.Context) error {
+		return controllers.DistanceSelectID(c)
+	})
+	e.GET("/api/distance/new", func(c echo.Context) error {
+		return controllers.DistanceSelectNew(c)
+	})
+	e.GET("/api/distance/macaddress", func(c echo.Context) error {
+		return controllers.DistanceSelectMacAddress(c)
 	})
 	// Start server
 	e.Logger.Fatal(e.Start(":3000"))
